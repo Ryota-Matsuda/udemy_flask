7f90b3ed0d41c6c3aa74379cdbd60eef2c0f3240
Ryota-Matsuda <m.ryota.0917@outlook.jp>
2024-06-16 19:53:10 +0900

    画面内での分岐処理を実装

--- myproject/venv/app.py
+++ myproject/venv/app.py
@@ -2,10 +2,21 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-
+list = [
+    'test1',
+    'test2',
+    'test3',
+    'test4',
+    'test5',
+    'test6'
+]
 @app.route("/")
 def hello_html():
     return render_template("index.html")
+
+@app.route("/<name>")
+def hello(name):
+    return render_template("index.html",name=name,list=list)
 
 if __name__ == "__main__":
     app.run(debug=True)
